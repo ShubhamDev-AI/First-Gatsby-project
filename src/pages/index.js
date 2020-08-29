23b57bf4a0f5f3d144d6e476c21001f463350aec
shubham zade <67915582+ShubhamDev-AI@users.noreply.github.com>
2020-08-29 15:52:03 +0530

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from 'gatsby';
+import Header from '../components/header';
+
+
+
+
+export default function Home() {
+  return (
+    <div style={{ color: `purple` }}>
+      <Header headerText="I am from Home"/>
+      <Link to="/about-css-modules"> AboutPage</Link>
+      <h1>Hello Gatsby!</h1>
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  );
+}
